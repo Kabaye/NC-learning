b75b3fd161f052aadc9029dd1d2f67da3b4361b7
svku0919 <svkulich@gmail.com>
2020-04-07 13:27:48 +0300

    feat: add beans

--- bpp/src/main/java/edu/netcracker/bpp/bean/Bean3.java
+++ bpp/src/main/java/edu/netcracker/bpp/bean/Bean3.java
@@ -1,8 +1,5 @@
 package edu.netcracker.bpp.bean;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public class Bean3 {
-    private boolean b3 = true;
+    private String s3 = "bean3";
 }
--- bpp/src/main/java/edu/netcracker/bpp/bean/Bean4.java
+++
@@ -1,8 +0,0 @@
-package edu.netcracker.bpp.bean;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Bean4 {
-    private String s4 = "Bean 4";
-}
