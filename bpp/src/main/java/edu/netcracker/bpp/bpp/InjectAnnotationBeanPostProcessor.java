2011d9e95867b1625e410e2db797accee5018363
svku0919 <svkulich@gmail.com>
2020-04-07 14:58:26 +0300

    fix: some fixes in BPP

--- bpp/src/main/java/edu/netcracker/bpp/bpp/InjectAnnotationBeanPostProcessor.java
+++ bpp/src/main/java/edu/netcracker/bpp/bpp/InjectAnnotationBeanPostProcessor.java
@@ -10,9 +10,7 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ReflectionUtils;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,12 +20,10 @@ import java.util.Objects;
 public class InjectAnnotationBeanPostProcessor implements BeanPostProcessor {
     private final ConfigurableListableBeanFactory configurableListableBeanFactory;
     private final HashMap<Pair<String, String>, String> beansWithInjection;
-    private final List<String> dependencies;
 
     public InjectAnnotationBeanPostProcessor(ConfigurableListableBeanFactory configurableListableBeanFactory) {
         this.configurableListableBeanFactory = configurableListableBeanFactory;
         beansWithInjection = new HashMap<>();
-        dependencies = new ArrayList<>();
     }
 
     @Override
@@ -53,29 +49,17 @@ public class InjectAnnotationBeanPostProcessor implements BeanPostProcessor {
                     ReflectionUtils.setField(field, bean, valueToInject);
                 }
             }
-            dependencies.clear();
         }
         return bean;
     }
 
     @SneakyThrows
     private Object findOrCreate(Class<?> clazz, String name) {
-        if (dependencies.contains(name)) {
-            throw new RuntimeException("There are some unresolved circular dependencies");
-        }
-        if (BeanFactoryUtils.beansOfTypeIncludingAncestors(configurableListableBeanFactory, clazz).size() != 0) {
+        final int size = BeanFactoryUtils.beansOfTypeIncludingAncestors(configurableListableBeanFactory, clazz).size();
+        if (size == 1) {
             return BeanFactoryUtils.beanOfTypeIncludingAncestors(configurableListableBeanFactory, clazz);
         } else {
-            dependencies.add(name);
-            Constructor<?> constructor = clazz.getConstructors()[0];
-            Parameter[] parameters = constructor.getParameters();
-            Object[] parameterValues = new Object[parameters.length];
-            for (int i = 0; i < parameters.length; i++) {
-                parameterValues[i] = findOrCreate(parameters[i].getType(), parameters[i].getName());
-            }
-            Object newBean = constructor.newInstance(parameterValues);
-            configurableListableBeanFactory.registerSingleton(name, newBean);
-            return newBean;
+            return configurableListableBeanFactory.getBean(name);
         }
     }
 }
