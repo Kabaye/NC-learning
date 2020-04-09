35398a727fe7fc77d15ace09e3554af17060d778
svku0919 <svkulich@gmail.com>
2020-04-09 15:50:37 +0300

    feat: add some tests

--- Reactive-learning-app/order-app/src/main/java/edu/netcracker/order/app/OrderAppApplication.java
+++ Reactive-learning-app/order-app/src/main/java/edu/netcracker/order/app/OrderAppApplication.java
@@ -2,12 +2,25 @@ package edu.netcracker.order.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Random;
 
 @SpringBootApplication
 public class OrderAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderAppApplication.class, args);
+        final Random random = new Random(System.currentTimeMillis());
+        Flux.interval(Duration.ofSeconds(1)).map(time -> {
+            final int nextInt = random.nextInt(200_000);
+            if (nextInt < 150_000) {
+                return nextInt;
+            }
+            throw new RuntimeException("Integer > 100_000");
+        }).subscribe(System.out::println,
+                System.out::println);
     }
 
 }
--- Reactive-learning-app/order-app/src/test/java/edu/netcracker/order/app/OrderAppApplicationTests.java
+++ Reactive-learning-app/order-app/src/test/java/edu/netcracker/order/app/OrderAppApplicationTests.java
@@ -1,13 +1,56 @@
 package edu.netcracker.order.app;
 
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootTest
 class OrderAppApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testFlux() {
+        List<Integer> elements = new ArrayList<>();
+        List<String> strs = new ArrayList<>();
+
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> i * 2)
+                .subscribeOn(Schedulers.parallel())
+                .subscribe(elements::add);
+        System.out.println(elements);
+    }
+
+    @Test
+    void testFlux2() {
+        Flux<Integer> ints = Flux.range(1, 10);
+        ints.subscribe(System.out::println);
+    }
+
+    @Test
+    @SneakyThrows
+    void testFlux3() {
+        final Random random = new Random(System.currentTimeMillis());
+        Flux.interval(Duration.ofSeconds(1)).map(time -> {
+            final int nextInt = random.nextInt(200_000);
+            if (nextInt < 150_000) {
+                return nextInt;
+            }
+            throw new RuntimeException("Integer > 100_000");
+        }).subscribe(System.out::println,
+                System.out::println);
+
+        Thread.sleep(1_000);
     }
 
 }
