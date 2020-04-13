198922f59a26cd4bea6f8f3c4dc37514ff3936a3
svku0919 <svkulich@gmail.com>
2020-04-13 14:11:34 +0300

    feat: add ProductService methods to controller

--- Reactive-learning-app/order-app/src/main/java/edu/netcracker/order/app/controller/OrderProductController.java
+++ Reactive-learning-app/order-app/src/main/java/edu/netcracker/order/app/controller/OrderProductController.java
@@ -2,19 +2,26 @@ package edu.netcracker.order.app.controller;
 
 import edu.netcracker.order.app.order.entity.Order;
 import edu.netcracker.order.app.order.service.OrderService;
+import edu.netcracker.order.app.product.entity.Product;
+import edu.netcracker.order.app.product.service.ProductService;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController("/api/v1")
 public class OrderProductController {
     private final OrderService orderService;
+    private final ProductService productService;
 
-    public OrderProductController(OrderService orderService) {
+    public OrderProductController(OrderService orderService, ProductService productService) {
         this.orderService = orderService;
+        this.productService = productService;
     }
 
     @GetMapping("/orders/{orderId}")
@@ -25,5 +32,31 @@ public class OrderProductController {
     @PostMapping("/orders")
     public Mono<Order> saveOrder(@RequestBody Order order) {
         return orderService.saveOrder(order);
+    }
+
+
+    @PostMapping("/products")
+    public Mono<Product> saveProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/products/{id}")
+    public Mono<Product> updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public Mono<Void> deleteProduct(@PathVariable Integer id) {
+        return productService.deleteProduct(id);
+    }
+
+    @GetMapping("/products")
+    public Flux<Product> findAll() {
+        return productService.findAllProducts();
+    }
+
+    @GetMapping("/products/{id}")
+    public Mono<Product> findProduct(@PathVariable Integer id) {
+        return productService.findProductById(id);
     }
 }
