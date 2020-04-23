87660ace6142cfaf6b9b16651a821485ffd0c045
svku0919 <svkulich@gmail.com>
2020-04-23 13:44:17 +0300

    feat: add metrics annotations and AspectJ annotation default config

--- Reactive-learning-app/customer-app/src/main/java/edu/netcracker/customer/app/CustomerAppApplication.java
+++ Reactive-learning-app/customer-app/src/main/java/edu/netcracker/customer/app/CustomerAppApplication.java
@@ -1,11 +1,11 @@
 package edu.netcracker.customer.app;
 
-import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@Getter
+@EnableAspectJAutoProxy
 public class CustomerAppApplication {
 
     public static void main(String[] args) {
