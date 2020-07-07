ac1d5583898f1b651c7fab8c3639bdfd47f17cad
Kabaye <svkulich@gmail.com>
2020-07-07 11:18:04 +0300

    feat: add Starter and Inclusion modules

---
+++ SpringBoot-Starter-learning-app/SpringBoot-Starter-inclusion/src/main/java/edu/netcracker/springboot/starter/inclusion/SpringBootStarterInclusionApplication.java
@@ -0,0 +1,13 @@
+package edu.netcracker.springboot.starter.inclusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterInclusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterInclusionApplication.class, args);
+    }
+
+}
---
+++ SpringBoot-Starter-learning-app/SpringBoot-Starter-inclusion/src/test/java/edu/netcracker/custom/swagger/starter/inclusion/SpringBootStarterInclusionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.netcracker.custom.swagger.starter.inclusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterInclusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
