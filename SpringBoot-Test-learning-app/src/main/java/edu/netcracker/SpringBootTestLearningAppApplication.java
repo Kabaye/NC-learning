65e3469bd079cdb99db62043c0093791a4d182ca
Kabaye <svkulich@gmail.com>
2020-07-23 10:42:35 +0300

    feat: initial commit for SpringBoot Test learning app

---
+++ SpringBoot-Test-learning-app/src/main/java/edu/netcracker/SpringBootTestLearningAppApplication.java
@@ -0,0 +1,13 @@
+package edu.netcracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestLearningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestLearningAppApplication.class, args);
+    }
+
+}
