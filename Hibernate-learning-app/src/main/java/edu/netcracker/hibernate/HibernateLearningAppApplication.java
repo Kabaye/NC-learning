1404cede92e8d4ee27d04ba158e3f1774134849c
Kabaye <svkulich@gmail.com>
2020-10-12 12:21:28 +0300

    Initital commit

---
+++ Hibernate-learning-app/src/main/java/edu/netcracker/hibernate/HibernateLearningAppApplication.java
@@ -0,0 +1,13 @@
+package edu.netcracker.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateLearningAppApplication.class, args);
+	}
+
+}
---
+++ Hibernate-learning-app/src/test/java/edu/netcracker/hibernate/HibernateLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.netcracker.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateLearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
