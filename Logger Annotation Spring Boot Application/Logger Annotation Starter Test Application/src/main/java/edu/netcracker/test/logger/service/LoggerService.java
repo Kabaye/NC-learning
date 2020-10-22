57c6f164b75e30955fb12c4ab5e957d95d7d9be4
Kabaye <svkulich@gmail.com>
2020-10-22 10:13:06 +0300

    feat: move Logger Annotation starter to Netcracker Git repository

--- Logger Annotation Spring Boot Application/Logger Annotation Starter Test Application/src/main/java/edu/netcracker/test/logger/service/LoggerService.java
+++
@@ -1,9 +0,0 @@
-package edu.netcracker.test.logger.service;
-
-/**
- * @author svku0919
- * @version 20.10.2020
- */
-public interface LoggerService {
-    String getRandomString(Integer intAmount);
-}
