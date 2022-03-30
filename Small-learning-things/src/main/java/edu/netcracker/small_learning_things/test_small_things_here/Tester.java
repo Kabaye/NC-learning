5673eaab3ca2dece69f555ac756f7901ee9a0765
Kabaye <svkulich@gmail.com>
2022-03-30 13:38:42 +0300

    feat: small changes

--- Small-learning-things/src/main/java/edu/netcracker/small_learning_things/test_small_things_here/Tester.java
+++ Small-learning-things/src/main/java/edu/netcracker/small_learning_things/test_small_things_here/Tester.java
@@ -1,33 +1,30 @@
 package edu.netcracker.small_learning_things.test_small_things_here;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tester {
+    private final static String s = """
+            40149,Sorry - you are not, authorised to create refund requests.
+            40150,Sorry - you are not authorised to export debt escalation requests.
+            """;
+
     public static void main(String[] args) {
-        ArrayList<Integer> ints = new ArrayList<>(Integer.MAX_VALUE / 2);
-        System.out.println(ints.size());
-        ArrayList<C> l = new ArrayList<>();
-//        doSmth(l);
+//        Pattern pattern = Pattern.compile("\\d+,.*");
+        Pattern pattern = Pattern.compile("^(\\d+),(.*)$");
+        System.out.println("Error Codes file parsing started");
+        s.lines().forEach(line -> {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.matches()) {
+//                String[] split = line.split(",");
+//                if (split.length > 2) {
+//                    split[1] = Arrays.stream(split).skip(2).reduce(split[1], (s1, s2) -> s1 + "," + s2);
+//                }
+//                System.out.println(split[0] + " ======= " + split[1]);
+                System.out.println(matcher.group(1) + " ======= " + matcher.group(2));
+            }
+        });
     }
 
-    static class A{
-        public String s;
-    }
-
-    static class B extends A{
-        public String ss;
-    }
-
-    static class C extends B{
-        public String sss;
-    }
-
-    public static List<? super B> doSmth(List<? super B> l){
-        l.add(new C());
-        ArrayList<C> cs = new ArrayList<>();
-        ArrayList<B> bs = new ArrayList<>();
-        ArrayList<A> as = new ArrayList<>();
-        return as;
-    }
 }
