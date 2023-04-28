e87025f60ef7d5cfd7567c91d5060c17a6d94dad
Kabaye <svkulich@gmail.com>
2023-04-28 18:03:38 +0300

    feat: small web app

--- quarkus-docker-demo/src/main/java/edu/netcracker/ExampleResource.java
+++ quarkus-docker-demo/src/main/java/edu/netcracker/ExampleResource.java
@@ -1,6 +1,7 @@
 package edu.netcracker;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.MurmurHash3;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -27,7 +28,7 @@ public class ExampleResource {
     @GET
     @Path("uuid")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response hello(@QueryParam("amount") Integer amount) {
+    public Response getUUIDS(@QueryParam("amount") Integer amount) {
         amount = Objects.requireNonNullElse(amount, applicationConfiguration.defaultAmount());
         log.info("Generating UUID. Amount = " + amount);
         return Response.ok().entity(Map.of("ids", Stream.generate(UUID::randomUUID)
@@ -35,4 +36,42 @@ public class ExampleResource {
                         .toList(), "amount", amount))
                 .build();
     }
+
+    @GET
+    @Path("hash")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response generateHash(@QueryParam("valueToHash") String valueToHash) {
+        return Response.ok().entity(Map.of("hash", getHash(valueToHash).toUpperCase()))
+                .build();
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789abcdef".toCharArray();
+
+    public static String getHash(String value) {
+        return bytesToHex(getHash(MurmurHash3.hash128x64(value.getBytes())[0]));
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    private static byte[] getHash(long v) {
+        byte[] bytes = new byte[8];
+        bytes[0] = (byte) v;
+        bytes[1] = (byte) (v >>> 8);
+        bytes[2] = (byte) (v >>> 16);
+        bytes[3] = (byte) (v >>> 24);
+        bytes[4] = (byte) (v >>> 32);
+        bytes[5] = (byte) (v >>> 40);
+        bytes[6] = (byte) (v >>> 48);
+        bytes[7] = (byte) (v >>> 56);
+        return bytes;
+    }
+
 }
